200faf62dd59f39d379cbd50a426b89880d91cdc
Alexandre Lins <aslinsjr@gmail.com>
2024-11-05 13:18:28 -0300

    v

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
+import { StrictMode, Children } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 import ErrorPage from './pages/ErrorPage.jsx'
 import SegundaPagina from './pages/SegundaPagina.jsx'
@@ -16,7 +16,7 @@ const routes = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        path: "/home",
         element: <Home />
       },
       {
@@ -24,7 +24,7 @@ const routes = createBrowserRouter([
         element: <SegundaPagina />
       },
     ]
-  },
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
