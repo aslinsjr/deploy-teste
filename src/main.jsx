9587fefff68e66e633741f026c035617d8f16ea1
Alexandre Lins <aslinsjr@gmail.com>
2024-11-06 06:14:19 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const routes = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/home",
+        path: "/",
         element: <Home />
       },
       {
